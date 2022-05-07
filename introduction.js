b81b725075f5c320f078400e21c23c218e9f5a07
blueishot <fabricioteixeirahungria@gmail.com>
2022-05-07 16:12:15 -0300

    console log

--- introduction.js
+++ introduction.js
@@ -3,3 +3,19 @@
 console.log('hello world');
 
 // JavaScript Variables and Constants
+
+let variable1 = 2;
+
+var variable2 = 3;
+let variable3 = 4;
+
+let variable4 = 5, variable5 = 6, variable7 = 7;
+
+// JavaScript console.log()
+
+console.log ('apenas uma mensagem');
+
+const variable8 = 'texto';
+const variable9 = 'informativo';
+
+console.log(variable8, '', variable9);
