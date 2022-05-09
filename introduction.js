d95ee2a2348f6b67f866a7f0e3c710b8aa09b5f6
blueishot <fabricioteixeirahungria@gmail.com>
2022-05-09 08:53:16 -0300

    Javascript Types

--- introduction.js
+++ introduction.js
@@ -19,3 +19,26 @@ const variable8 = 'texto';
 const variable9 = 'informativo';
 
 console.log(variable8, '', variable9);
+
+//JavaScript Data Types
+
+const variableString = 'Uma string'; // String
+const variableNumber = 12 // Number
+const variableBigInt = 900719925124740998n // BigInt
+const variableBoolean = true // Boolean
+const variableUndefined = undefined // Undefined
+const variableNull = null // Null
+const variableSymbol = Symbol('Hello') // Symbol
+const variableObject = {
+    bandName: 'Engelbert Humperdinck',
+    songName: 'A Man Without Love'
+} // Object
+
+console.log(typeof(variableString));
+console.log(typeof(variableNumber));
+console.log(typeof(variableBigInt));
+console.log(typeof(variableBoolean));
+console.log(typeof(variableUndefined));
+console.log(typeof(variableNull));
+console.log(typeof(variableSymbol));
+console.log(typeof(variableObject));
