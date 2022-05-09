06c97ff6cc23da521169055f7153972cc42c5769
blueishot <fabricioteixeirahungria@gmail.com>
2022-05-09 13:32:10 -0300

    if else statement

---
+++ controlFlow.js
@@ -0,0 +1,22 @@
+// JavaScript if...else Statement
+
+variable10 = 15
+
+if (variable10 > 8) {
+    console.log('A variável é maior que 8');
+} else {
+    console.log('A variável não é maior que 8')
+}
+
+if (variable10 == 8) {
+    console.log('A variável é 8');
+} else if (variable10 == 9) {
+    console.log('A variável é 9')
+} else {
+    console.log('A variável é qualquer outra')
+}
+
+if (variable10 == 15)
+    console.log('é 15')
+else
+    console.log('não é 15')
