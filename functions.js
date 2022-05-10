3d90f305f8113c3203b695c6a06818baedb327a7
blueishot <fabricioteixeirahungria@gmail.com>
2022-05-10 09:49:12 -0300

    Javascript Functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+// JavaScript Function and Function Expressions
+
+function example () {
+    console.log('Eu sou uma função example');
+}
+
+example();
+
+function calcular(num) {
+    return num + 10;
+}
+
+console.log(calcular(10));
+
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiplicar(10, 4));
