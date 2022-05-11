c251b4bfa2792aaa94e31778df02a5a4126b4a33
blueishot <fabricioteixeirahungria@gmail.com>
2022-05-11 15:01:00 -0300

    array

---
+++ types.js
@@ -0,0 +1,38 @@
+// JavaScript Arrays
+
+const someArray = ['eu', 'sou', 'uma', 'array'];
+
+const someArray2 = new Array('eu', 'também', 'sou'); // Não recomendado 
+
+const someArray3 = [
+    {'pais': 'França'},
+    [1, 2, 3],
+    function arrayFunction() {
+        return console.log('sou uma function dentro de uma array')
+    }
+];
+
+console.log(someArray[1]);
+console.log(someArray3[0])
+
+const somethingMissing = ['Javascript', 'Kotlin', 'Python']
+somethingMissing.push('Java');
+
+console.log(somethingMissing[3]);
+somethingMissing.unshift('Ruby');
+console.log(somethingMissing[3]);
+console.log(somethingMissing[0]);
+
+console.log(somethingMissing);
+somethingMissing.pop();
+console.log(somethingMissing);
+somethingMissing.shift();
+console.log(somethingMissing)
+
+const arrayExample1 = [1, 2, 3];
+const arrayExample2 = [4, 5, 6];
+
+const concatExample = arrayExample1.concat(arrayExample2);
+console.log(concatExample);
+
+const indexExample
