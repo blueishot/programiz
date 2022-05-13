5610f2278c67c07c24968fda32a8e4477986ad8b
blueishot <fabricioteixeirahungria@gmail.com>
2022-05-13 14:20:43 -0300

    Arrow Functions

--- ES6.js
+++ ES6.js
@@ -23,3 +23,33 @@ let { id, idade, banco } = someObject;
 console.log(id);
 console.log(idade);
 console.log(banco);
+
+// JavaScript Arrow Function
+
+let arrowExample = () => {
+    console.log('Eu sou uma arrow function')
+}
+
+arrowExample();
+
+let arrowExample2 = (argumento) => {
+    if(argumento > 5) {
+        return console.log('O argumento é maior que 5')
+    } else {
+        return console.log('O argumento não é maior que 5')
+    }
+}
+
+arrowExample2(10);
+
+function callArgs() {
+    console.log(arguments)
+}
+
+callArgs(10, 20, 30);
+
+let arrowExample3 = (...args) => {
+    console.log(args);
+} // Spread is needed
+
+arrowExample3(10, 20, 30)
